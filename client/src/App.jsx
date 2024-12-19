053b86eceae7eee24927d9c17f45e3f96d0b1884
AkashAmrolkar <akashamrolkar10@gmail.com>
2024-12-19 21:44:31 +0530

    project setup

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from './components/components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-red'>Akash Amrolkar</h1>
+      <Button className=" bg-red-400"/>
+    </>
+  )
+}
+
+export default App
